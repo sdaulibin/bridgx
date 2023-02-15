bdd9cb7dcbb73233272facd7469a4b3be80f8a30
binginx <sdaulibin@126.com>
2023-02-15 17:50:30 +0800

    [Task][TencentCloud] Oprate LB Support TencentCloud #235 CreateLoadBalancer CreateListener

--- pkg/cloud/tencent/lb.go
+++ pkg/cloud/tencent/lb.go
@@ -2,20 +2,55 @@ package tencent
 
 import (
 	"errors"
+	"fmt"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 
 	"github.com/galaxy-future/BridgX/pkg/cloud"
+
+	clb "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/clb/v20180317"
 )
 
 func (p *TencentCloud) CreateLoadBalancer(req cloud.CreateLoadBalancerRequest) (cloud.CreateLoadBalancerResponse, error) {
-	// you can call api like this
-	// p.clbClient.CreateLoadBalancer()
-	// TODO implement me
-	return cloud.CreateLoadBalancerResponse{}, errors.New("implement me")
+	if req.LoadBalancerName == "" {
+		return cloud.CreateLoadBalancerResponse{}, fmt.Errorf("loadBalancer name is empty")
+	}
+	var (
+		loadBalancerType = "INTERNAL"
+		subnetId         = "subnet-8up9sesx"
+	)
+	request := clb.NewCreateLoadBalancerRequest()
+	request.LoadBalancerName = &req.LoadBalancerName
+	request.LoadBalancerType = &loadBalancerType
+	request.SubnetId = &subnetId
+	response, err := p.clbClient.CreateLoadBalancer(request)
+	if err != nil {
+		return cloud.CreateLoadBalancerResponse{}, err
+	}
+	if response.Response.LoadBalancerIds == nil || len(response.Response.LoadBalancerIds) == 0 {
+		return cloud.CreateLoadBalancerResponse{}, fmt.Errorf("loadBalancer ids length 0 or nil")
+	}
+	return cloud.CreateLoadBalancerResponse{
+		LoadBalancerId: *response.Response.LoadBalancerIds[0],
+	}, nil
 }
 
 func (p *TencentCloud) CreateListener(req cloud.CreateListenerRequest) error {
-	// TODO implement me
-	return errors.New("implement me")
+	request := clb.NewCreateListenerRequest()
+	request.LoadBalancerId = &req.LoadBalancerId
+	request.Protocol = (*string)(&req.Protocol)
+	ports := make([]*int64, 0)
+	for _, v := range req.PortList {
+		ports = append(ports, common.Int64Ptr(int64(v)))
+	}
+	request.Ports = ports
+	response, err := p.clbClient.CreateListener(request)
+	if err != nil {
+		return err
+	}
+	if response.Response.ListenerIds == nil || len(response.Response.ListenerIds) == 0 {
+		return fmt.Errorf("Listener ids length 0 or nil")
+	}
+	return nil
 }
 
 func (p *TencentCloud) RegisterBackendServer(req cloud.RegisterBackendServerRequest) error {
--- pkg/cloud/tencent/lb_test.go
+++ pkg/cloud/tencent/lb_test.go
@@ -1,1 +1,76 @@
 package tencent
+
+import (
+	"github.com/galaxy-future/BridgX/pkg/cloud"
+	"testing"
+)
+
+var (
+	ak = ""
+	sk = ""
+)
+
+func TestTencentCloud_CreateLoadBalancer(t *testing.T) {
+	type args struct {
+		request cloud.CreateLoadBalancerRequest
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    cloud.CreateLoadBalancerResponse
+		wantErr bool
+	}{
+		{
+			name: "测试新建lb",
+			args: args{
+				request: cloud.CreateLoadBalancerRequest{
+					LoadBalancerName: "test001",
+				},
+			},
+			wantErr: false,
+		},
+	}
+	p, _ := New(ak, sk, "ap-beijing")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := p.CreateLoadBalancer(tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TestTencentCloud CreateLoadBalancer() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			t.Logf("TestTencentCloud CreateLoadBalancer() got = %v", got)
+		})
+	}
+}
+
+func TestTencentCloud_CreateListener(t *testing.T) {
+	type args struct {
+		req cloud.CreateListenerRequest
+	}
+
+	tests := []struct {
+		name    string
+		agrs    args
+		wantErr bool
+	}{
+		{
+			name: "新建listener",
+			agrs: args{req: cloud.CreateListenerRequest{
+				LoadBalancerId: "lb-hccgj54f",
+				Protocol:       cloud.ProtocolTypeHTTP,
+				PortList:       []int{8088, 8089},
+			}},
+		},
+	}
+	p, _ := New(ak, sk, "ap-beijing")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := p.CreateListener(tt.agrs.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("TestTencentCloud CreateListener() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
